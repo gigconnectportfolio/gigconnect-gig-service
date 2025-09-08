a82681dd96ef8b70fb0da9a5fd8eca00f2b55868
Keith <karirukeith@gmail.com>
2025-09-08 17:26:48 +0300

    implemented schema and model

---
+++ src/models/gig.schema.ts
@@ -0,0 +1,49 @@
+import {model, Model, Schema} from "mongoose";
+import {ISellerGig} from "@kariru-k/gigconnect-shared";
+
+const gigSchema: Schema<ISellerGig> = new Schema(
+    {
+        sellerId: {type: Schema.Types.ObjectId, index: true},
+        username: {type: String, required: true},
+        profilePicture: {type: String, required: true},
+        email: {type: String, required: true},
+        title: {type: String, required: true},
+        description: {type: String, required: true},
+        basicTitle: {type: String, required: true},
+        basicDescription: {type: String, required: true},
+        categories: {type: String, required: true},
+        subCategories: [{type: String, required: true}],
+        tags: [{type: String}],
+        active: {type: Boolean, default: true},
+        expectedDelivery: {type: String, default: ''},
+        ratingsCount: {type: Number, default: 0},
+        ratingSum: {type: Number, default: 0},
+        ratingCategories: {
+            five: { value: {type: Number, default: 0}, count: {type: Number, default: 0} },
+            four: { value: {type: Number, default: 0}, count: {type: Number, default: 0} },
+            three: { value: {type: Number, default: 0}, count: {type: Number, default: 0} },
+            two: { value: {type: Number, default: 0}, count: {type: Number, default: 0} },
+            one: { value: {type: Number, default: 0}, count: {type: Number, default: 0} },
+        },
+        price: {type: Number, default: 0},
+        sortId: {type: Number},
+        coverImage: {type: String, required: true},
+        createdAt: {type: Date, default: Date.now()},
+    },
+    {
+        versionKey: false,
+        toJSON: {
+            transform(_doc, ret) {
+                ret.id = ret._id;
+                delete (ret as any)._id;  // 👈 TS is fine with this
+                return ret;
+            }
+        }
+    }
+)
+
+gigSchema.virtual('id').get(function (){
+    return this._id;
+});
+
+export const gigModel: Model<ISellerGig> = model<ISellerGig>('Gig', gigSchema, 'Gig');
