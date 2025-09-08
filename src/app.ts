4f98304fd88e0cd56cfb69fdc27f95cc5f71310a
Keith <karirukeith@gmail.com>
2025-09-09 00:52:18 +0300

    implemented redis connection method

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import {databaseConnection} from "./database";
 import {config} from "./config";
 import express, {Express} from "express";
 import {start} from "./server";
+import {redisConnect} from "./redis/redis.connection";
 
 const initialize = (): void => {
     config.cloudinaryConfig();
     databaseConnection();
     const app: Express = express();
     start(app);
+    redisConnect();
 }
 
 initialize();
---
+++ src/redis/redis.connection.ts
@@ -0,0 +1,19 @@
+import {winstonLogger} from "@kariru-k/gigconnect-shared";
+import {Logger} from "winston";
+import {createClient} from "redis";
+import {config} from "../config";
+
+const log: Logger = winstonLogger('Redis Connection', 'Gig Service Redis Connection', 'debug');
+type RedisClientType = ReturnType<typeof createClient>;
+
+const client: RedisClientType = createClient({ url: `${config.REDIS_HOST}`});
+
+export const redisConnect = async (): Promise<void> => {
+    try {
+        client.on('error', (err) => log.error('Redis Client Error', err));
+        await client.connect();
+        log.info(`✅ Gig Service Connected To Redis Successfully: ${await client.ping()}`);
+    } catch (error) {
+        log.error('❌ Gig Service Redis Connection Error', error);
+    }
+}
