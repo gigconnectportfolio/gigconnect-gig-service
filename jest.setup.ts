9bfae91bc7daccd26fb31c2979b61c05c31e2ad0
Keith <karirukeith@gmail.com>
2026-04-13 15:23:09 +0300

    mocks for redis and elasticsearch added

--- jest.setup.ts
+++ jest.setup.ts
@@ -12,6 +12,8 @@ jest.mock('@kariru-k/gigconnect-shared');
 jest.mock('src/elasticsearch');
 jest.mock('src/schemes/gig');
 jest.mock('@elastic/elasticsearch');
+jest.mock('src/redis/redis.connection');
+jest.mock('src/redis/gig.cache.ts');
 
 
 jest.mock('uuid', () => ({
